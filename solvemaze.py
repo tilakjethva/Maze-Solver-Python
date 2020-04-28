5a2f18fcafdc3edf0fd04badc5731771685a3e5c
Sundaran Kumar <daran9@gmail.com>
2020-04-28 11:24:13 +0200

    Updated python project

--- solvemaze.py
+++ solvemaze.py
@@ -1,7 +1,11 @@
 import sys
 
-
-class MazeSolver:
+# Authors
+# Group 22
+# Chendursundaran KUMARAGURUBARAN
+# Jenoh JOHNSON
+# Tilak Chandrakantbhai JETHVA
+class AmazeSolver:
 
     def __init__(self, input_file):
         self.input_file = input_file
@@ -12,6 +16,7 @@ def main():
         print("Please provide an input maze file as argument!")
     else:
         input_file = sys.argv[1]
+    solver = AmazeSolver(input_file)
 
 
 if __name__ == "__main__":
